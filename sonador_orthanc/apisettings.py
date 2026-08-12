e82963dfa060e1894bfefb5c196857cbb83d93f2
Rob Oakes <rob.oakes@oak-tree.us>
2026-08-12 19:21:21 +0000

    Set branch version to 0.4-dev

--- sonador_orthanc/apisettings.py
+++ sonador_orthanc/apisettings.py
@@ -12,7 +12,7 @@ from client import apisettings as gapi
 # `/system` endpoint (see `sonador_orthanc.web.system.SonadorOrthancSystemReportView`), alongside
 # `SonadorUrl`; the Sonador viewer surfaces it in its About table as the "Imaging Server Sonador
 # Cloud Plugin Version".
-VERSION = '0.4.1'
+VERSION = '0.4-dev'
 
 # Sonador Cache Index Status Codes
 SONADOR_CACHE_STATUS_CURRENT = 'current'
